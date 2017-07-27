6f08929d4d437014bac70070e3093a9fbb9b0584
Bitworks, Ltd <bitworks@bw-sw.com>
2017-07-27 12:48:38 +0700

    upgraded run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sed -i 's/$inactive/'"${NGINX_CACHE_TIME}"'/g' /etc/nginx/nginx.conf
-sed -i 's/$rate_limit/'"${NGINX_RATE_LIMIT}"'/g' /etc/nginx/nginx.conf
+sed -i "s/\$inactive/${NGINX_CACHE_TIME}/g" /etc/nginx/nginx.conf
+sed -i "s/\$rate_limit/${NGINX_RATE_LIMIT}/g" /etc/nginx/nginx.conf
 
 /usr/sbin/nginx ; java -jar /opt/bin/jetty.jar --path /pulse /var/lib/jetty/webapps/cs-pulse-server.war
